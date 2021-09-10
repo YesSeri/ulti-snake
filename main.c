374ab6281c810b0a06dccc6405fbe1a5ebe225be
yesseri <henrik.zenkert@gmail.com>
2021-09-10 11:55:55 +0200

    early stages

--- main.c
+++ main.c
@@ -5,14 +5,20 @@
 #define __USE_POSIX199309
 #define _POSIX_C_SOURCE 199309L
 #define SIZE 15
+
+int nanosleep(const struct timespec *req, struct timespec *rem);
 void update();
 void paint();
+void set_food();
 void sleep_us(unsigned long microseconds);
 
 int main()
 {
     // False means no food.
-    bool arr[SIZE][SIZE] = { false };
+    bool board[SIZE][SIZE] = { false };
+    paint(board);
+    set_food(1, 8, board);
+    paint(board);
 
     // for (;;) {
     //     printf("aaa\n");
@@ -22,11 +28,21 @@ int main()
     // }
     return 0;
 }
+void set_food(int x, int y, bool board[SIZE][SIZE]){
+    board[y][x] = true;
+}
 void update()
 {
 }
-void paint()
+void paint(bool boardp[SIZE][SIZE])
 {
+    for(int x = 0; x < SIZE; x++){
+        for(int y = 0; y < SIZE; y++){
+            printf(boardp[y][x] ? "*" : "-");
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 void sleep_ms(unsigned long millis)
 {
